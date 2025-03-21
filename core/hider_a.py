32c52995045d024e58cdfb0337ee6533abfa0923
jiink <jacob.j.carlson03@gmail.com>
2025-03-21 14:37:59 -0700

    goes to random pos in shadow if not in shadow

--- core/hider_a.py
+++ core/hider_a.py
@@ -5,10 +5,18 @@ class HiderA(Npc):
     # This function runs periodically. This is where the algorithm should
     # figure out where to go. See the parent class to see what's available.
     def think(self):
-        self.emit_thought("thinking...")
-        # Random spot
-        while True:
-            x = random.randint(0, self.grid.size - 1)
-            y = random.randint(0, self.grid.size - 1)
-            if self.set_target(x, y):
-                break
+        
+        # if it's already in an unseen area, don't do anything
+        here = self.grid.get_node(*self.position.to_grid_pos())
+        if here is not None and not here.seen_by_seeker:
+            self.emit_thought("I'm fine.")
+            return
+        self.emit_thought("Gotta run!")
+        candidate_pos = []
+        for node in self.grid.all_nodes():
+            if not node.seen_by_seeker:
+                candidate_pos.append(node.get_position())
+        if len(candidate_pos) > 0:
+            # pick a random spot 
+            new_pos = random.choice(candidate_pos)
+            self.set_target(*new_pos)
---
+++ core/hider_random.py
@@ -0,0 +1,14 @@
+import random
+from core.npc import Npc
+
+class HiderRandom(Npc):
+    # This function runs periodically. This is where the algorithm should
+    # figure out where to go. See the parent class to see what's available.
+    def think(self):
+        self.emit_thought("thinking...")
+        # Random spot
+        while True:
+            x = random.randint(0, self.grid.size - 1)
+            y = random.randint(0, self.grid.size - 1)
+            if self.set_target(x, y):
+                break
