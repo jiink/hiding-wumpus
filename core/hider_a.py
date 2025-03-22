e3e7d99c55dc79f8cc68aafb32c36a905ec33e65
Jacob C <jacob.j.carlson03@gmail.com>
2025-03-21 22:05:45 -0700

    wip

--- core/hider_a.py
+++ core/hider_a.py
@@ -25,8 +25,17 @@ class HiderA(Npc):
         for node in self.grid.all_nodes():
             if not node.is_wall and not node.seen_by_seeker:
                 # associate each candidate node with a score
-                score = self.position.distance_to(node.get_position()) / self.grid.size
-                self.candidates.append((node, score))
+                # Big part of it is how deep in the shadow it is -- that is,
+                # going to a spot that's far from a visible area. I found out
+                # This is an uninformed search problem called "distance transform"!
+                # It can be done with BFS. A naive way would be to find the distance from
+                # every shadowed node to the nearest seen node by doing a BFS on each 
+                # shadowed node, but a better way is to do a BFS from every seen node simulatneously.
+                # This is not as hard as it sounds, as all it means is that I would make the 
+                # queue start off not with nothing, but with all the nodes seen by the seeker.
+                shadow_depth_score = self.grid.distance_from_seen(node)
+                distance_score = self.position.distance_to(node.get_position()) / self.grid.size
+                self.candidates.append((node, shadow_depth_score + distance_score))
         
         # want to pick the candidate with the lowest score
         self.candidates.sort(key=lambda x: x[1], reverse=False) # ascending
