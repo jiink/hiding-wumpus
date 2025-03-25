f03a3d82c394c39be0a9ee5bc653a828038dc632
jiink <jacob.j.carlson03@gmail.com>
2025-03-25 13:11:10 -0700

    untested bfs

--- core/hider_a.py
+++ core/hider_a.py
@@ -1,16 +1,56 @@
+import queue
 import random
+from typing import List, Tuple
 
 import pygame
 from core.npc import Npc
+from models.grid_node import GridNode
 
 class HiderA(Npc):
-
+    
     def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             # this is an instance variable so we can see it 
             # drawn to the screen in think_draw()
             self.candidates = [] # (node, score)
+            self.shadow_depths = [] # (node, depth)
     
+    # Finds the distances between every shadowed node and the nearest node seen
+    # by the seeker and stores it in self.shadow_depths
+    def calculate_shadow_depths(self) -> None:
+        # this is multi-source BFS
+        # good resource: https://medium.com/geekculture/multisource-bfs-for-your-faang-coding-interviews-d5177753f507
+        que = queue.Queue() # could use [], but this has faster operations I heard
+        all_nodes = self.grid.all_nodes()
+        num_nodes = len(self.grid.all_nodes)
+        # Store distance from each node to a seen node. it's a dictionary!
+        distances: dict[GridNode, int] = {}
+        INF: int = 9999999
+        for i in range(0, len(all_nodes)):
+            distances[all_nodes[i]] = INF
+        # Initialize the queue. usually in BFS we would start the queue with one
+        # node, but we can just start it will all the nodes. Let the queue
+        # begin with all seen nodes.
+        for node in all_nodes:
+            if node.seen_by_seeker:
+                queue.put(node)
+                distances[node] = 0
+        dist = 0
+        while not que.empty():
+            node = que.get()
+            dist += 1
+            for n in self.grid.get_neighbors(node):
+                if distances[n] == INF:
+                    distances[n] = dist
+                    que.put(n)
+        # TODO: reflect calculation in self.shadow_depths
+
+
+    # Returns the distance from the given node to the nearest node seen by the 
+    # seeker.
+    def distance_from_seen(self, node: GridNode) -> int:
+        pass
+
     # This function runs periodically. This is where the algorithm should
     # figure out where to go. See the parent class to see what's available.
     def think(self):
@@ -33,7 +73,7 @@ class HiderA(Npc):
                 # shadowed node, but a better way is to do a BFS from every seen node simulatneously.
                 # This is not as hard as it sounds, as all it means is that I would make the 
                 # queue start off not with nothing, but with all the nodes seen by the seeker.
-                shadow_depth_score = self.grid.distance_from_seen(node)
+                shadow_depth_score = self.distance_from_seen(node)
                 distance_score = self.position.distance_to(node.get_position()) / self.grid.size
                 self.candidates.append((node, shadow_depth_score + distance_score))
         
